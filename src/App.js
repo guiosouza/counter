f377ac0c89396079ec0602e5171fb1176e4911e3
unknown <guilhermedeoliveiradesouza@gmail.com>
2024-08-12 17:09:24 -0300

    added days

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import "./App.css";
+import "@fontsource/inter";
+import CustomStepper from "./components/CustomStepper";
+import { Grid } from "@mui/joy";
+import * as React from "react";
+import Card from "@mui/joy/Card";
+import CardContent from "@mui/joy/CardContent";
+import LinearProgressWithLabel from "./components/LinearProgressWithLabel";
+import MediaCover from "./components/MediaCover";
+
+function App() {
+  const [progress, setProgress] = React.useState(0);
+
+  return (
+    <Grid container spacing={3} sx={{ flexGrow: 1 }}>
+      <Grid xs={12} sx={{ display: "flex", justifyContent: "center" }} mt={4}>
+        <Card variant="outlined">
+          <CardContent>
+            <CustomStepper progress={progress} />
+          </CardContent>
+        </Card>
+      </Grid>
+      <Grid xs={12} sx={{ display: "flex", justifyContent: "center" }} mt={4} p={4}>
+        <MediaCover />
+      </Grid>
+      <Grid xs={12} sx={{ display: "flex", justifyContent: "center" }} mt={0} p={4}>
+        <LinearProgressWithLabel onProgress={setProgress} />
+      </Grid>
+    </Grid>
+  );
+}
+
+export default App;
---
+++ src/components/CustomStepper.jsx
@@ -0,0 +1,87 @@
+import * as React from "react";
+import Stepper from "@mui/joy/Stepper";
+import Step, { stepClasses } from "@mui/joy/Step";
+import StepIndicator, { stepIndicatorClasses } from "@mui/joy/StepIndicator";
+import Typography, { typographyClasses } from "@mui/joy/Typography";
+import CheckRoundedIcon from "@mui/icons-material/CheckRounded";
+import SdCardAlert from "@mui/icons-material/SdCardAlert";
+
+export default function CustomStepper({ progress }) {
+  const steps = [
+    { label: "Day 2", day: 2 },
+    { label: "Day 6", day: 6 },
+    { label: "Day 17", day: 17 },
+    { label: "Day 25", day: 25 },
+    { label: "Day 32", day: 32 },
+    { label: "Day 90", day: 90 },
+    { label: "Day 111", day: 111 },
+    { label: "Day 120", day: 120 },
+  ];
+
+  const currentDay = Math.floor((progress / 100) * 120); // Progresso em dias
+
+  return (
+    <Stepper
+      orientation="vertical"
+      sx={{
+        "--Stepper-verticalGap": "2.5rem",
+        "--StepIndicator-size": "2.5rem",
+        "--Step-gap": "1rem",
+        "--Step-connectorInset": "0.5rem",
+        "--Step-connectorRadius": "1rem",
+        "--Step-connectorThickness": "4px",
+        "--joy-palette-success-solidBg": "var(--joy-palette-success-400)",
+        [`& .${stepClasses.completed}`]: {
+          "&::after": { bgcolor: "success.solidBg" },
+        },
+        [`& .${stepClasses.active}`]: {
+          [`& .${stepIndicatorClasses.root}`]: {
+            border: "4px solid",
+            borderColor: "#fff",
+            boxShadow: (theme) =>
+              `0 0 0 1px ${theme.vars.palette.primary[500]}`,
+          },
+        },
+        [`& .${stepClasses.disabled} *`]: {
+          color: "neutral.softDisabledColor",
+        },
+        [`& .${typographyClasses["title-sm"]}`]: {
+          textTransform: "uppercase",
+          letterSpacing: "1px",
+          fontSize: "10px",
+        },
+      }}
+    >
+      {steps.map((step, index) => (
+        <Step
+          key={index}
+          completed={currentDay >= step.day}
+          active={
+            currentDay < step.day &&
+            (index === 0 || currentDay >= steps[index - 1].day)
+          }
+          indicator={
+            currentDay >= step.day ? (
+              <StepIndicator variant="solid" color="success">
+                <CheckRoundedIcon />
+              </StepIndicator>
+            ) : currentDay < step.day && steps[index - 1] && currentDay >= steps[index - 1].day ? (
+              <StepIndicator variant="solid" color="primary">
+                <SdCardAlert />
+              </StepIndicator>
+            ) : (
+              <StepIndicator variant="solid" color="neutral">
+                <CheckRoundedIcon />
+              </StepIndicator>
+            )
+          }
+        >
+          <div>
+            <Typography level="title-sm">Step {index + 1}</Typography>
+            {step.label}
+          </div>
+        </Step>
+      ))}
+    </Stepper>
+  );
+}
